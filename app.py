b90673b0c8be5200e07f63bd9c4c048dc7bd1963
pavani-1510 <pavanikangundi@gmail.com>
2026-02-09 09:58:31 +0530

    v1.0.0

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template_string
 app = Flask(__name__)
 
 # Read the HTML file content
-with open('brick_breaker.html', 'r', encoding='utf-8') as f:
+with open('home.html', 'r', encoding='utf-8') as f:
     html_content = f.read()
 
 @app.route('/')
